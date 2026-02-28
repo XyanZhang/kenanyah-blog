cbaa7a840197afee2a012d7764dfb7aac1de1fc2
张鑫炎 <zhangxinyan@testbird.com>
2026-02-28 16:14:15 +0800

    perf: 页面切换动效优化

---
+++ apps/web/src/app/(main)/loading.tsx
@@ -0,0 +1,8 @@
+import { PageLoading } from '@/components/layout/PageLoading'
+
+/**
+ * (main) 路由组 loading：在博客 / 关于 / 相册等子页面间切换时显示。
+ */
+export default function MainLoading() {
+  return <PageLoading />
+}
