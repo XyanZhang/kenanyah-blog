98e54ae0ed4f231fdf979d7d4df91c20856615b7
XyanZhang <1743067175@qq.com>
2026-03-04 23:44:52 +0800

    feat: 图片墙样式方式重构

--- apps/web/src/app/(main)/pictures/page.tsx
+++ apps/web/src/app/(main)/pictures/page.tsx
@@ -18,11 +18,21 @@ const mockPictures: PictureStackItem[] = [
   { id: '6', src: 'https://picsum.photos/seed/p6/440/360', date: '2026-02-01' },
   { id: '7', src: 'https://picsum.photos/seed/p7/440/360', date: '2026-01-25' },
   { id: '8', src: 'https://picsum.photos/seed/p8/440/360', date: '2026-01-18' },
+  { id: '9', src: 'https://picsum.photos/seed/p9/440/360', date: '2026-01-12' },
+  { id: '10', src: 'https://picsum.photos/seed/p10/440/360', date: '2026-01-05' },
+  { id: '11', src: 'https://picsum.photos/seed/p11/440/360', date: '2025-12-28' },
+  { id: '12', src: 'https://picsum.photos/seed/p12/440/360', date: '2025-12-20' },
+  { id: '13', src: 'https://picsum.photos/seed/p13/440/360', date: '2025-12-14' },
+  { id: '14', src: 'https://picsum.photos/seed/p14/440/360', date: '2025-12-08' },
+  { id: '15', src: 'https://picsum.photos/seed/p15/440/360', date: '2025-12-01' },
+  { id: '16', src: 'https://picsum.photos/seed/p16/440/360', date: '2025-11-24' },
+  { id: '17', src: 'https://picsum.photos/seed/p17/440/360', date: '2025-11-18' },
+  { id: '18', src: 'https://picsum.photos/seed/p18/440/360', date: '2025-11-10' },
 ]
 
 export default function PicturesPage() {
   return (
-    <main className="h-[calc(100vh-80px)] w-full flex flex-col">
+    <main className="h-[calc(100vh-6.25rem)] w-full flex flex-col">
       <div className="flex-1 flex flex-col min-h-0 w-full">
         <div className="flex-1 min-h-0 min-w-0">
           <PictureStack items={mockPictures} className="h-full" />
